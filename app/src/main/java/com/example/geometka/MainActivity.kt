9cf19416ddf6f4ab7afdc47f9eaee75107eb0632
revqueenge <Dealul02@gmail.com>
2026-04-12 15:31:38 +0700

    base visual

--- app/src/main/java/com/example/geometka/MainActivity.kt
+++ app/src/main/java/com/example/geometka/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.geometka
 
 import android.app.Activity
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.Toast
@@ -13,21 +16,35 @@ class MainActivity : Activity() {
         val layout = LinearLayout(this).apply {
             orientation = LinearLayout.VERTICAL
             setPadding(50, 50, 50, 50)
+            setBackgroundColor(Color.parseColor("#1E1E1E"))
+            gravity = Gravity.CENTER
+        }
+
+        val buttonBackground = GradientDrawable().apply {
+            shape = GradientDrawable.RECTANGLE
+            setColor(Color.parseColor("#2196F3"))
+            cornerRadius = 60f
         }
 
         val button = Button(this).apply {
-            text = "gg"
+            text = "Отставить метку"
             textSize = 18f
+            setTextColor(Color.WHITE)
+            background = buttonBackground
 
             layoutParams = LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT
-            )
+            ).apply {
+                topMargin = 1300
+            }
+
+            setPadding(80, 40, 80, 40)
 
             setOnClickListener {
                 Toast.makeText(
                     this@MainActivity,
-                    "Geometka работает!",
+                    "Метка отставлена!",
                     Toast.LENGTH_SHORT
                 ).show()
             }
